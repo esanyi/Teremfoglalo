a3b476b9c0e46db7c5f9a2f95b5e8baba02d276d
esanyi <e.sanyi@gmail.com>
2013-05-28 00:21:31 +0300

    Create TeremType.java

---
+++ TeremType.java
@@ -0,0 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beadando1;
+
+/**
+ *
+ * @author egyed.sandor_1
+ */
+public enum TeremType {eloado, szeminarium, gepterem};
